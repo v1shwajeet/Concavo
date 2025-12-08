afd75a683a07c616da562d1b8534f7742d33f666
v1shwajeet <vishwajeetgehlot3@gmail.com>
2025-12-08 19:13:42 +0530

    Implement Navbar

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NewCompanion = () => {
   return (
     <div>
-      
+      NewCompanion
     </div>
   )
 }
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Subscription = () => {
   return (
     <div>
-      
+      Subscription
     </div>
   )
 }
