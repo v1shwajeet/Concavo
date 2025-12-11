c47b6a8282afd305828e06bc1cc7d3a8034a15ec
v1shwajeet <vishwajeetgehlot3@gmail.com>
2025-12-11 18:19:14 +0530

    card created via params

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,12 +1,35 @@
-import CompanionCard from "@/components/CompanionCard"
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 
-const CompanionsLibrary = () => {
+
+const CompanionsLibrary = async ({searchParams}:SearchParams) => {
+  const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
+
+    const companions = await getAllCompanions({ subject, topic });
+
+    // console.log(companions)
+
   return (
-    <div>
-      <CompanionCard/>
-      <CompanionCard/>
-      <CompanionCard/>
-    </div>
+    <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    Filter
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
   )
 }
 
