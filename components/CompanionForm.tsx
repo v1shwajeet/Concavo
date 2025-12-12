08d2f9900cfe477a2b3cac4d687463245774df7c
v1shwajeet <vishwajeetgehlot3@gmail.com>
2025-12-12 22:53:28 +0530

    fix typescript error

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -36,7 +36,7 @@ const formSchema = z.object({
 
 const CompanionForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+    resolver: zodResolver(formSchema) as any,
     defaultValues: {
       name: "",
       subject: "",
